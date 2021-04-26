ff2f293fc167980b2dd008e2d22852dc4862ad94
lazauninke <sandrajonikaite@gmail.com>
2021-04-26 11:56:52 +0300

    dog

---
+++ gyvunai/Dog.js
@@ -0,0 +1,17 @@
+
+const Pet = require('./Pet.js');
+
+/*
+Dog:
+- parametrus: name, color, sound
+- metodai: voice, introduce
+*/
+
+class Dog extends Pet {
+    constructor(name, color) {
+        super(name, color);
+        this.sound = 'Au au';
+    }
+}
+
+module.exports = Dog;
